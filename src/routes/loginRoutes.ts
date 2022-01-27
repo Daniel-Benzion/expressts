920a5e6e360055a4ff7bf0251e76d3ba3ae7b13e
Daniel Benzion <daniel.z.benzion@gmail.com>
2022-01-26 22:25:24 -0600

    parsing form bodies

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -1,9 +1,21 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.send('Hi there!');
+router.get('/', (req: Request, res: Response) => {
+    res.send(`
+        <form method="POST">
+            <div>
+                <label>Email</label>
+                <input name="email" />
+            </div>
+                <label>Password</label>
+                <input name="password" type="password" />
+            <div>
+            <input type="submit" />
+            </div>
+        </form>
+    `);
 });
 
 export { router };
